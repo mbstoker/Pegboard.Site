ba81e304830d4a038c0b19be82391813c7e86cbf
stokerm1971 <stoker_mike@hotmail.com>
2026-05-25 13:43:42 +0100

    updated hero images

--- src/PegboardWebSite/Program.cs
+++ src/PegboardWebSite/Program.cs
@@ -35,7 +35,7 @@ public class Program
         // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
         {
-            app.UseExceptionHandler("/Error");
+             app.UseExceptionHandler("/Error");
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
